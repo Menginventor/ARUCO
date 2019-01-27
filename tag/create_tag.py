7425880efe221418709210df4dc13f710ae9c755
Menginventor <meng.inventor@gmail.com>
2019-01-27 20:56:11 +0700

    tag

--- tag/create_tag.py
+++ tag/create_tag.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from cv2 import aruco
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-marker_id = 1
+marker_id = 0
 marker_width_meter = 0.15#meter
 
 printer_DPI = 72
@@ -11,4 +11,4 @@ marker_width_pixel = int(marker_width_meter*meter_to_inch*printer_DPI)
 
 img = aruco.drawMarker(	aruco_dict, marker_id, marker_width_pixel,None,1)
 
-cv2.imwrite('marker.bmp',img)
+cv2.imwrite('marker_id%d.bmp'%marker_id,img)
